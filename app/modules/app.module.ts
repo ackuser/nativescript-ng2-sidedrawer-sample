7324265089d0ff0226672b4dd31450e4b31aef55
Karim <kabdeselam@synergicpartners.com>
2017-08-26 10:11:31 +0200

    Refactoring sidedrawer

--- app/modules/app.module.ts
+++ app/modules/app.module.ts
@@ -1,25 +1,23 @@
 import { NgModule } from '@angular/core';
 
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { NativeScriptUISideDrawerModule } from 'nativescript-telerik-ui/sidedrawer/angular/side-drawer-directives';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { SharedModule } from './shared/shared.module';
+import { SideDrawerPageComponent } from './side-drawer-page/side-drawer-page.component';
 
 @NgModule({
   imports: [
     NativeScriptModule,
     AppRoutingModule,
-    SharedModule
+    NativeScriptUISideDrawerModule
   ],
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent,
-    ContactComponent
+    SideDrawerPageComponent
   ],
   bootstrap: [AppComponent]
 })
